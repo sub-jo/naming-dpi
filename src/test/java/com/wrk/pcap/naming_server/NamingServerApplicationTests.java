77f6f908c0ca91ec3fb8cb352f3c9befe38330a9
sub-jo <yusuf.abdelmoneam@vodafone.com>
2025-08-13 19:43:49 +0200

    add spring default project

---
+++ src/test/java/com/wrk/pcap/naming_server/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrk.pcap.naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
